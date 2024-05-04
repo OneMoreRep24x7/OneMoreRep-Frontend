e175ece22fbab42de1fd310ca21323d8ca5cc088
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-05-04 09:37:17 +0530

    add route gaurd

---
+++ src/app/Guards/guards.ts
@@ -0,0 +1,12 @@
+import { Inject, inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
+
+export const AuthGuard: CanActivateFn = ()=> {
+
+    const router = inject(Router)
+    if(sessionStorage.getItem('user')){
+        return true;
+    }
+    router.navigate(['/login'])
+    return false;
+}
